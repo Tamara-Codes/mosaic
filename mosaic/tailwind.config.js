a62a9253f64b363461a2bdd63ddc1178274fb859
Tamara <codewithtamara@gmail.com>
2025-07-25 22:08:33 +0200

    tailwind

---
+++ mosaic/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",   // adjust if your code lives elsewhere
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
