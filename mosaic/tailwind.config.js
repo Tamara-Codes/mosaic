204b0b4b091246cdaf7aa3e74cae492650963204
Tamara <codewithtamara@gmail.com>
2025-07-26 02:01:05 +0200

    add login component

--- mosaic/tailwind.config.js
+++ mosaic/tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{ts,tsx,js,jsx}",
+    "./components/**/*.{ts,tsx,js,jsx}",
+    "./src/**/*.{ts,tsx,js,jsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
