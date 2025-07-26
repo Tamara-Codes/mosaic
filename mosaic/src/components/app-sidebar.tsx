6a7d341b0b1e340e3f8e7bf36ccdc61cb1bfd238
Tamara <codewithtamara@gmail.com>
2025-07-26 16:51:23 +0200

    add workflows

--- mosaic/src/components/app-sidebar.tsx
+++ mosaic/src/components/app-sidebar.tsx
@@ -22,24 +22,62 @@ export function AppSidebar() {
         <SidebarGroup>
           <SidebarMenu>
             <SidebarMenuItem>
-              <SidebarMenuButton>
-                Workflows
-              </SidebarMenuButton>
+              <SidebarMenuButton>Workflows</SidebarMenuButton>
 
               <SidebarMenuSub>
                 <SidebarMenuSubItem>
                   <SidebarMenuSubButton asChild>
-                    <Link href="/workflows/lead_generator">Lead Generator</Link>
+                    <Link href="/workflows/news-reporter">News Reporter</Link>
                   </SidebarMenuSubButton>
                 </SidebarMenuSubItem>
                 <SidebarMenuSubItem>
                   <SidebarMenuSubButton asChild>
-                    <Link href="/workflows/talent_finder">Talent Finder</Link>
+                    <Link href="/workflows/content-creator">Content Creator</Link>
                   </SidebarMenuSubButton>
                 </SidebarMenuSubItem>
                 <SidebarMenuSubItem>
                   <SidebarMenuSubButton asChild>
-                    <Link href="/workflows/review_master">Reiew Master</Link>
+                    <Link href="/workflows/review-master">Review Master</Link>
+                  </SidebarMenuSubButton>
+                </SidebarMenuSubItem>
+                <SidebarMenuSubItem>
+                  <SidebarMenuSubButton asChild>
+                    <Link href="/workflows/guest-journey">Guest Journey</Link>
+                  </SidebarMenuSubButton>
+                </SidebarMenuSubItem>
+                <SidebarMenuSubItem>
+                  <SidebarMenuSubButton asChild>
+                    <Link href="/workflows/client-concierge">Client Concierge</Link>
+                  </SidebarMenuSubButton>
+                </SidebarMenuSubItem>
+                <SidebarMenuSubItem>
+                  <SidebarMenuSubButton asChild>
+                    <Link href="/workflows/social-media-scraper">Social Media Scraper</Link>
+                  </SidebarMenuSubButton>
+                </SidebarMenuSubItem>
+                <SidebarMenuSubItem>
+                  <SidebarMenuSubButton asChild>
+                    <Link href="/workflows/company-researcher">Company Researcher</Link>
+                  </SidebarMenuSubButton>
+                </SidebarMenuSubItem>
+                <SidebarMenuSubItem>
+                  <SidebarMenuSubButton asChild>
+                    <Link href="/workflows/talent-finder">Talent Finder</Link>
+                  </SidebarMenuSubButton>
+                </SidebarMenuSubItem>
+                <SidebarMenuSubItem>
+                  <SidebarMenuSubButton asChild>
+                    <Link href="/workflows/receipt-tracker">Receipt Tracker</Link>
+                  </SidebarMenuSubButton>
+                </SidebarMenuSubItem>
+                <SidebarMenuSubItem>
+                  <SidebarMenuSubButton asChild>
+                    <Link href="/workflows/lead-generator">Lead Generator</Link>
+                  </SidebarMenuSubButton>
+                </SidebarMenuSubItem>
+                <SidebarMenuSubItem>
+                  <SidebarMenuSubButton asChild>
+                    <Link href="/workflows/campaign-sender">Campaign Sender</Link>
                   </SidebarMenuSubButton>
                 </SidebarMenuSubItem>
               </SidebarMenuSub>
